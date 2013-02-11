ef451695ed346e2647dea885572433002b336c06
Mirko Leschikar <fafnirx@gmail.com>
2013-02-11 17:50:39 +0100

    Added Task model

--- src/main/java/de/fafnirx/tasktracker/bean/TasksBean.java
+++ src/main/java/de/fafnirx/tasktracker/bean/TasksBean.java
@@ -9,6 +9,7 @@ import de.fafnirx.tasktracker.entity.Task;
 import de.fafnirx.tasktracker.model.TaskModel;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -17,10 +18,11 @@ import javax.inject.Named;
  * @author Mirko Leschikar <fafnirx@gmail.com>
  */
 @Named
-@RequestScoped
+@ViewScoped
 public class TasksBean {
     private List<Task> allTasks;
     private Task currentTask;
+    private TaskModel taskModel;
     
     @Inject
     TasksDao taskDao;
@@ -34,14 +36,25 @@ public class TasksBean {
     public TaskModel getAllTasks() {
         if (allTasks == null) {
             allTasks = taskDao.findAllTasks();
+            taskModel = new TaskModel( allTasks, taskDao);
         }
         
-        return new TaskModel( allTasks, taskDao);
+        return taskModel;
     }
     public Task getSelectedTask() {
         return currentTask;
     }
+    
     public void setSelectedTask( Task task) {
         currentTask = task;
+    }
+    
+    public void updateTask() {
+        String name = getSelectedTask().getName();
+        System.out.println("Name = " + name);
+    }
+    
+    public void createTask() {
+        
     }
 }
